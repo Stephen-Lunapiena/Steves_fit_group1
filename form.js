8e7b26bfb22fc27fe102bd803d037295125319de
Stephen Lunapiena <stephen.lunapiena@gmail.com>
2021-08-25 22:43:43 -0400

    made api fetch remove display

--- form.js
+++ form.js
@@ -4,24 +4,25 @@ var x = document.querySelector("#emailform")
 
   function submitForm(e) {
       e.preventDefault()
-      let name = document.querySelector("#name").value;
+      let name = document.querySelector("#name").value; //these are the individual values from my form. Declared as variables to use later on
       let email = document.querySelector("#email").value;
       let subject = document.querySelector("#subject").value;
       let msg = document.querySelector("#msg").value;
       sendEmail(name, subject, email, msg)
       console.log(email)
-  
+
+  //smtp function template
   function sendEmail(name, subject, email, msg) {
       Email.send({
           Host: "smtp.gmail.com",
-          Username : "projectest120@gmail.com",
+          Username : "projectest120@gmail.com", 
           Password : "randompw",
           To : 'projectest120@gmail.com',
           From : "Stephen.lunapiena@gmail.com",
           Subject : name + " has sent you a message",
           Body : `Name: ${name} \rEmail: ${email} \rFitness goals: ${subject}\rMessage: ${msg}`
       }).then(
-        m => alert(m)
+         alert("your form has been submitted")
       )};
   }
 
--- main.js
+++ main.js
@@ -1,45 +1,42 @@
 const form = document.getElementById('nutrition_form')
-const dataUl = document.querySelector("#resultsList")
+const dataDiv = document.querySelector("#resultsList")
 
-function display_stuff(obj) {
-  console.log(obj)
-    var newLi = document.createElement("li")
-    newLi.appendChild(document.createTextNode(`${obj.label}: ${obj.quantity} ${obj.unit}s`))
-    dataUl.appendChild(newLi)
+
+function display_stuff(obj, container) {
+
+    var newLi = document.createElement("li") //creating elements
+    newLi.setAttribute("class", "newLi") //setting class to element just created
+
+    newLi.appendChild(document.createTextNode(`${obj.label}: ${obj.quantity} ${obj.unit}s`)) //adds text to "li". Taking object properties that I need from data
+    container.appendChild(newLi) 
+    dataDiv.appendChild(container)
   }
 
 form.addEventListener('submit', (event) => {
   event.preventDefault()
 
   var food_ingredient = document.querySelector('#food_ingredient').value
-  console.log(food_ingredient);
-
-
+  
   fetch(`https://api.edamam.com/api/nutrition-data?app_id=a5be03de&app_key=fc3f3e2c95599671a870ac7e93f0059a&nutrition-type=logging&ingr=${food_ingredient}`)
 
   .then(response => response.json())
-  .then(data => {  
+  .then(data => {
     
+    var findOldUl = document.querySelector("#dataUl")
+
+    if(findOldUl !== null) findOldUl.remove() //if an old ul is found, pressing send button removes it, if not...
+
+    var newUl = document.createElement("ul") //creates a new ul
+    newUl.setAttribute("id", "dataUl")
+  
     const nutrients = data.totalNutrientsKCal
 
-    display_stuff(nutrients.ENERC_KCAL)
-    display_stuff(nutrients.PROCNT_KCAL)
-    display_stuff(nutrients.FAT_KCAL)
-    display_stuff(nutrients.CHOCDF_KCAL)
-    
+    display_stuff(nutrients.ENERC_KCAL, newUl) //new ul is being sent to displaystuff() as an argument to keep it in scope
+    display_stuff(nutrients.PROCNT_KCAL, newUl)
+    display_stuff(nutrients.FAT_KCAL, newUl)
+    display_stuff(nutrients.CHOCDF_KCAL, newUl)
+
   })
   .catch(error => console.log(error))
 
   })
-
-
-
-
-
-
-
-
-
-
-
-  
